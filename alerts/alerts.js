7044ab3a6033912c982a34bd799b0c30ff8e0e97
moobynoob <37037062+moobynoob@users.noreply.github.com>
2018-04-15 15:53:44 +0800

    Updated minor changes

--- alerts/alerts.js
+++ alerts/alerts.js
@@ -10,27 +10,11 @@ var getArray = [];
 var allDisease = [];
 
 
-module.exports.generateAlerts = function(callback){
-    try{
-      con.query("SELECT Disease_Name, drank, dnum_of_cases, id, No_of_cases FROM diseases", function(err, alerts){
-        if(err){console.log("Error in alerts.js SELECT drank" + err);return callback(NULL);};
-//callback(drank_result);
-        createAlerts(alerts);
-
-        })
-
-  }
-
-
-
-  catch(e){
-    console.log(e);
-  }
-}
 
 function createAlerts(alerts){
   var cnt = 0;
   drank_result = [];
+  console.log(alerts);
   for(cnt;cnt<5;cnt++){
     allDisease.push(alerts[cnt]);
     if(alerts[cnt].drank > 0 && alerts[cnt].drank < 6){
@@ -64,27 +48,71 @@ function createAlerts(alerts){
     getArray = drank_result;
 
 }
-module.export.getAlerts = function(callback){
-  console.log("Alerts:");
-  let diseaseList = getAllDisease();
-  if(getArray.length !== 5){
-    createAlerts(diseaseList);
-  }
-//  con.end();
-  return {
-    diseases: diseaseList,
-    alerts: getArray
-  }
+//   module.export.getAlerts = function(callback){
+//   console.log("Alerts:");
+//   let diseaseList = getAllDisease();
+//   if(getArray.length !== 5){
+//     createAlerts(diseaseList);
+//   }
+// //  con.end();
+//   return {
+//     diseases: diseaseList,
+//     alerts: getArray
+//   }
+// }
+
+module.exports = {
+
+  generateAlerts: function(){
+      try{
+        con.query("SELECT Disease_Name, drank, dnum_of_cases, id, No_of_cases FROM diseases", function(err, alerts){
+          if(err){console.log("Error in alerts.js SELECT drank" + err);return callback(NULL);};
+  //callback(drank_result);
+          createAlerts(alerts);
+
+          })
+    }catch(e){
+        console.log(e);
+      }
+    },
+    getAlerts: function(){
+      console.log("Alerts:");
+      // console.log(getArray,getArray.length);
+      let diseaseList = getAllDisease();
+      console.log(diseaseList)
+      // if(diseaseList == undefined)
+      //   diseaseList = JSON.stringify(allDisease);
+      if(getArray.length !== 5){
+        createAlerts(diseaseList);
+      }
+    //  con.end();
+      return {
+        diseases: diseaseList,
+        alerts: getArray
+      }
+    }
 }
+
 var getAllDisease = function() {
   if (allDisease.length === 5) {
-    return allDisease;
+    console.log("IN IFFFF");
+    return allDisease.splice(0,5);
   } else {
     try{
+      console.log("IN ELSE");
       con.query("SELECT Disease_Name, drank, dnum_of_cases, id, No_of_cases FROM diseases", function(err, diseaseDetails){
         if(err){console.log("Error in alerts.js SELECT drank" + err);return callback(NULL);};
-        return JSON.stringify(diseaseDetails);
-      }
-    })
+        // console.log("Disease", JSON.stringify(diseaseDetails));
+        let allDiseaseStr = JSON.stringify(diseaseDetails);
+        console.log("allDisease",allDiseaseStr.length);
+        // while(allDiseaseStr.length == 0);
+        //     setTimeout(()=>console.log("still not got data"),500);
+        return allDisease;
+
+        // setTimeout(()=> JSON.stringify(diseaseDetails),2000);
+      })
+    }catch(e){
+        console.log(e);
+    }
   }
 }
